18615f990db84244f3d49f72ba3683a4285f7934
dennislam4 <107871794+dennislam4@users.noreply.github.com>
2022-06-27 14:56:10 -0600

    Initial commit

---
+++ num_guess.py
@@ -0,0 +1,26 @@
+# Author: Dennis Lam
+# GitHub username: dennislam4
+# Date: 6/27/2022
+# Description: Asks user for an integer that the player will try to guess. If the player
+#              guess is higher than the target integer, the program prints "too high" and
+#              vice versa. The program repeats until the correct integer is guessed. The
+#              program will then print the amount of tries that it took.
+
+integer = int(input("Enter the integer for the player to guess."))
+print("Enter your guess.")
+tries = 0
+while True:
+    guess = input()
+    guess = int(guess)
+    if guess < integer:
+        tries = tries + 1
+        print("Too low - try again:")
+    elif guess > integer:
+        tries = tries + 1
+        print("Too high - try again:")
+    else:
+        print("You guessed it in", tries, "tries.")
+        break
+
+
+
